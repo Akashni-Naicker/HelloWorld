47d34380e7774b06ebf0a2ed98683fd2c0e032b3
Akashni-Naicker <68015961+Akashni-Naicker@users.noreply.github.com>
2021-05-20 14:43:19 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld
         public static void main(String[] args) 
         {
             System.out.println("Hello World");
-            System.out.println("This is me!");
+            System.out.println("This is me! Hi!");
         }
     
 }
